9ff367f4705e81a602ce896c0ebd7b12e6a78545
martin heusser <50239556+mozziemozz@users.noreply.github.com>
2022-07-17 20:04:29 +0200

    add website files

---
+++ assets/scripts/config.js
@@ -0,0 +1,14 @@
+DataConnectors = [
+];
+
+var PageFabricConfig = {
+disqusId : "",
+googleAnalyticsId : "",
+globalSearchMode : "Webservice",
+globalSearchWebServiceURL : "",
+globalSearchLanguageFiles:[
+{
+ lang: "en", 
+ url: "/en.json"
+}]
+};
